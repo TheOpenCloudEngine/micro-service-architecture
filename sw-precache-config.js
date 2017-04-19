089479b111b60689cd864f1982f22d30d931e6d1
SeungpilPark <sppark@uengine.org>
2017-04-19 17:20:27 +0900

    #2 Iam 연동

---
+++ sw-precache-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  staticFileGlobs: [
+    'index.html',
+    'manifest.json',
+    'bower_components/webcomponentsjs/webcomponents-lite.min.js',
+    'images/default-receipt.png'
+  ],
+  navigateFallback: 'index.html'
+};
